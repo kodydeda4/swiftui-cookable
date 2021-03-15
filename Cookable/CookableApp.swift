8b3ca812934e5ef5f081bd1af41afaf59810baf0
Kody Deda <kodydeda4@gmail.com>
2021-03-15 07:04:11 -0400

    Initial Commit

---
+++ Cookable/CookableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookableApp.swift
+//  Cookable
+//
+//  Created by Kody Deda on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CookableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
