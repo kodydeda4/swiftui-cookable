2e0fd7943f1d02b3f9392a56519aeb2abf4651c0
Kody Deda <kodydeda4@gmail.com>
2021-04-06 07:22:58 -0400

    fixed clear all button not showing alert

--- Cookable/Views/Favorites/FavoritesTabView.swift
+++ Cookable/Views/Favorites/FavoritesTabView.swift
@@ -29,7 +29,7 @@ struct FavoritesTabView: View {
                     .toolbar {
                         ToolbarItem {
                             Button("Clear All") {
-                                viewStore.send(.clearFavoritesAlertConfirmed)
+                                viewStore.send(.clearFavoritesAlert)
                             }
                         }
                     }
